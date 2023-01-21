28090403c009e28e7b371a988b7d3ec907d91e62
LeoSolari <95233443+LeoSolari@users.noreply.github.com>
2023-01-20 23:24:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const d = document;
+
+// ACCORDION
+
+const accordion = d.getElementsByClassName("accordion_content_box");
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
+
+// CAROUSEL
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = d.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
